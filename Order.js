a493dbe8651c9381e79d1ad8ebd4cb1b14b96f55
EddieBissellSimmons <144965190+EddieBissellSimmons@users.noreply.github.com>
2025-03-01 01:45:38 +0000

    finished the assignment!

--- Order.js
+++ Order.js
@@ -4,37 +4,65 @@ export class Order {
         WELCOMING: () => {
           let aReturn = [];
           this.stateCur = this.OrderState.RESERVINGPIZZA;
-          aReturn.push("Welcome to Eddie's Pizzaria. Our menu today is pepperoni, veggie, or canadian pizza. What kind of pizza would you like?");
+          aReturn.push("Welcome to Eddies Pizzaria. Our menu today is pepperoni, veggie, or canadian pizza. What kind of pizza would you like?");
           return aReturn;
         },
         RESERVINGPIZZA: (sInput) => {
           let aReturn = [];
-          this.stateCur = this.OrderState.ORDERINGSIDE;
+          this.stateCur = this.OrderState.SELECTINGSIZE;
           if (sInput.toLowerCase().startsWith('c')) {
-            aReturn.push(`Canadian pizza! Excellent choice. Would you like to add wings to that?`);
+            aReturn.push(`Canadian pizza! Excellent choice. What size do you want that (small, medium, large)?`);
           } else if (sInput.toLowerCase().startsWith('p')) {
-            aReturn.push(`Pepperoni pizza! Excellent choice. Would you like to add wings to that?`);
+            aReturn.push(`Pepperoni pizza! Excellent choice. What size do you want that (small, medium, large)?`);
           } else if (sInput.toLowerCase().startsWith('v')) {
-            aReturn.push(`Veggie pizza! Excellent choice. Would you like to add wings to that?`);
+            aReturn.push(`Veggie pizza! Excellent choice. What size do you want that (small, medium, large)?`);
           } else {
             aReturn.push("Sorry we didn't quite get that.");
             aReturn.push("Please refresh and try again.");
           }
           return aReturn;
         },
-        ORDERINGSIDE: (sInput) => {
+        SELECTINGSIZE: (sInput) => {
           let aReturn = [];
-          this.isDone = true;
-          if (sInput.toLowerCase().startsWith('y')) {
-            aReturn.push(`Wings!`);
-          } else if (sInput.toLowerCase().startsWith('n')){
-            aReturn.push('Ok. Would you like anything else?');
+          this.stateCur = this.OrderState.SELECTCRUST;
+          if (sInput.toLowerCase().startsWith('s')) {
+            aReturn.push(`Small. Would you like normal or thin crust?`);
+          } else if (sInput.toLowerCase().startsWith('m')){
+            aReturn.push('Medium. Would you like normal or thin crust?');
+          } else if (sInput.toLowerCase().startsWith('l')){
+            aReturn.push('Large. Would you like normal or thin crust?');
           } else {
             aReturn.push("Sorry we didn't quite get that.");
             aReturn.push("Please refresh and try again.");
           }
           return aReturn;
-        }
+        },
+        SELECTCRUST: (sInput) => {
+          let aReturn = [];
+          this.stateCur = this.OrderState.ADDINGWINGS;
+          if (sInput.toLowerCase().startsWith('n')) {
+            aReturn.push(`Normal crust! Would you like to add our signature spicy BQQ wings for an extra $4.99??`);
+          } else if (sInput.toLowerCase().startsWith('t')){
+            aReturn.push('Thin crust! Would you like to add our signature spicy BQQ wings for an extra $4.99??');
+          } else {
+            aReturn.push("Sorry we didn't quite get that.");
+            aReturn.push("Please refresh and try again.");
+          }
+          return aReturn;
+        },
+        ADDINGWINGS: (sInput) => {
+          let aReturn = [];
+          this.isDone = true;
+          if (sInput.toLowerCase().startsWith('y')) {
+            aReturn.push(`Wings were added to your order! Your order has been placed! That you for ordering from Eddies Pizzaria, we hope you enjoy.`);
+          } else if (sInput.toLowerCase().startsWith('n')){
+            aReturn.push('No wings were added to your order. Your order has been placed! That you for ordering from Eddies Pizzaria, we hope you enjoy.');
+          } else {
+            aReturn.push("Sorry we didn't quite get that.");
+            aReturn.push("Please refresh and try again.");
+          }
+          return aReturn;
+        },
       };
   
       this.stateCur = this.OrderState.WELCOMING;
--- spec/order_spec.js
+++ spec/order_spec.js
@@ -4,31 +4,64 @@ describe("Tests all stages of an order", function() {
     it("test hello", function() {
         const oOrder = new Order("999-999-9999");
         const aResults = oOrder.handleInput("hello");
-        expect(aResults[0]).toBe("Welcome to Eddie's Pizzaria. Our menu today is pepperoni, veggie, or canadian pizza. What kind of pizza would you like?");
+        expect(aResults[0]).toBe("Welcome to Eddies Pizzaria. Our menu today is pepperoni, veggie, or canadian pizza. What kind of pizza would you like?");
     });
-    it("test yes", function() {
+    it("test pepperoni", function() {
         const oOrder = new Order("999-999-9999");
         oOrder.handleInput("hello");
         const aResults = oOrder.handleInput("pepperoni");
-        expect(aResults[0]).toBe("Pepperoni pizza! Excellent choice. Would you like to add wings to that?");
+        expect(aResults[0]).toBe("Pepperoni pizza! Excellent choice. What size do you want that (small, medium, large)?");
     });
-    it("test no", function() {
+    it("test veggie", function() {
         const oOrder = new Order("999-999-9999");
         oOrder.handleInput("hello");
         const aResults = oOrder.handleInput("veggie");
-        expect(aResults[0]).toBe("Veggie pizza! Excellent choice. Would you like to add wings to that?");
+        expect(aResults[0]).toBe("Veggie pizza! Excellent choice. What size do you want that (small, medium, large)?");
     });
-    it("test no", function() {
+    it("test canadian", function() {
         const oOrder = new Order("999-999-9999");
         oOrder.handleInput("hello");
         const aResults = oOrder.handleInput("canadian");
-        expect(aResults[0]).toBe("Canadian pizza! Excellent choice. Would you like to add wings to that?");
+        expect(aResults[0]).toBe("Canadian pizza! Excellent choice. What size do you want that (small, medium, large)?");
     });
-    it("test yes", function() {
+    it("test small", function() {
         const oOrder = new Order("999-999-9999");
-        oOrder.handleInput("hello");
-        const aResults = oOrder.handleInput("pepperoni");
-        expect(aResults[0]).toBe("Pepperoni pizza! Excellent choice. Would you like to add wings to that?");
+        const aResults = oOrder.handleInput("small");
+        expect(aResults[0]).toBe("Small. Would you like normal or thin crust?");
+    });
+    it("test medium", function() {
+        const oOrder = new Order("999-999-9999");
+        const aResults = oOrder.handleInput("medium");
+        expect(aResults[0]).toBe("Medium. Would you like normal or thin crust?");
+    });
+    it("test small", function() {
+        const oOrder = new Order("999-999-9999");
+        const aResults = oOrder.handleInput("small");
+        expect(aResults[0]).toBe("Small. Would you like normal or thin crust?");
+    });
+    it("test large", function() {
+        const oOrder = new Order("999-999-9999");
+        const aResults = oOrder.handleInput("large");
+        expect(aResults[0]).toBe("Large. Would you like normal or thin crust?");
+    });
+    it("test normal crust", function() {
+        const oOrder = new Order("999-999-9999");
+        const aResults = oOrder.handleInput("normal");
+        expect(aResults[0]).toBe("Normal crust! Would you like to add our signature spicy BQQ wings for an extra $4.99??");
+    });
+    it("test thin crust", function() {
+        const oOrder = new Order("999-999-9999");
+        const aResults = oOrder.handleInput("thin");
+        expect(aResults[0]).toBe("Thin crust! Would you like to add our signature spicy BQQ wings for an extra $4.99??");
+    });
+    it("test wing true", function() {
+        const oOrder = new Order("999-999-9999");
         const aResults = oOrder.handleInput("yes");
-        expect(aResults[0]).toBe("Wings!");
+        expect(aResults[0]).toBe("Wings were added to your order! Your order has been placed! That you for ordering from Eddies Pizzaria, we hope you enjoy.");
+    });
+    it("test wing false", function() {
+        const oOrder = new Order("999-999-9999");
+        const aResults = oOrder.handleInput("no");
+        expect(aResults[0]).toBe("No wings were added to your order. Your order has been placed! That you for ordering from Eddies Pizzaria, we hope you enjoy.");
+    });
     });
